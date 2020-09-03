64bf1befddb98f24f8dc25e257b3845b13412939
OlehSuprun <2548sov@gmail.com>
2020-09-04 01:42:01 +0300

    add image dialog

--- app/src/main/java/com/gmail2548sov/basicprojectrecyclerview/TelPickerFragment.kt
+++ app/src/main/java/com/gmail2548sov/basicprojectrecyclerview/TelPickerFragment.kt
@@ -3,11 +3,15 @@ package com.gmail2548sov.basicprojectrecyclerview
 import android.app.AlertDialog
 import android.app.Dialog
 import android.os.Bundle
+import android.view.LayoutInflater
 import androidx.fragment.app.DialogFragment
+import java.util.*
 
 class TelPickerFragment: DialogFragment() {
     override fun onCreateDialog(savedInstanceState: Bundle?): Dialog {
+        val view = LayoutInflater.from(activity).inflate(R.layout.image_dialog, null)
         return AlertDialog.Builder(activity)
+            .setView(view)
             .setTitle(R.string.tel_picker_title)
             .setPositiveButton(android.R.string.ok,null)
             .create()
