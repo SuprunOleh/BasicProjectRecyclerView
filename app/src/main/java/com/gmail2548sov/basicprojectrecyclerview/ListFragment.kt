0c7becb2882c36791c83eef8e56a703022ab370d
OlehSuprun <2548sov@gmail.com>
2020-10-12 02:20:33 +0300

    add subtitle

--- app/src/main/java/com/gmail2548sov/basicprojectrecyclerview/ListFragment.kt
+++ app/src/main/java/com/gmail2548sov/basicprojectrecyclerview/ListFragment.kt
@@ -1,50 +1,82 @@
 package com.gmail2548sov.basicprojectrecyclerview
 
+import android.content.Context
 import android.content.Intent
 import android.os.Bundle
 import android.util.Log
 import android.view.*
 import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
-import kotlinx.android.synthetic.main.fragment_user.view.*
-import kotlinx.android.synthetic.main.item_user.*
 import kotlinx.android.synthetic.main.item_user.view.*
-import kotlinx.android.synthetic.main.item_user.view.photo
-import java.util.zip.Inflater
+import java.util.*
+import kotlin.collections.ArrayList
 
 class ListFragment : Fragment() {
 
+    companion object {
+        final val SAVED_SUBTITLE_VISIBLE:String = "subtitle"
+
+    }
+
     lateinit var mRecyclerView: RecyclerView
     var mUserAdapter: UsersAdapter? = null
+    var mSubtitleVisible: Boolean = false
 
 
     override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
         super.onCreateOptionsMenu(menu, inflater)
         inflater.inflate(R.menu.fragment_list,menu)
+        val subTitleItem: MenuItem = menu.findItem(R.id.new_item)
+        if (mSubtitleVisible) subTitleItem.setTitle(R.string.hide_subtitle)
+        else subTitleItem.setTitle(R.string.show_subtitle)
     }
 
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
         Log.d("menu333", "asdfasd ")
 
-
         when (item.itemId) {
             R.id.new_user -> {
                 val user: DatfClass = DatfClass()
                 SingltonUsers.addUser(user)
-                Log.d("menu333", "asdfasd ${user.toString()}")
+//                Log.d("menu333", "asdfasd ${user.toString()}")
                 val intent = UserPagerActivity.newIntent(context, user.id)
-
                 startActivity(intent)
-
-
+//                updateUI()
+                return true
+            }
+            R.id.new_item -> {
+                Toast.makeText(context, "Item User", Toast.LENGTH_SHORT).show()
+                mSubtitleVisible = !mSubtitleVisible
+                activity?.invalidateOptionsMenu()
+                updateSubtitle()
                 return true
             }
             else -> return super.onOptionsItemSelected(item)
         }
+    }
+
+    fun updateSubtitle(){
+
+        val userCount = SingltonUsers.mListUsers.size
+        var subtitle:String? = getString(R.string.subtitle_format, userCount)
+        //val activity_us = activity
+
+        if (!mSubtitleVisible) {subtitle = null}
+
+        val activity_user: AppCompatActivity = activity as AppCompatActivity
+        activity_user.supportActionBar?.setSubtitle(subtitle)
 
     }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        outState.putBoolean(SAVED_SUBTITLE_VISIBLE, mSubtitleVisible)
+    }
+
+
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -59,6 +91,9 @@ class ListFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         val view: View = inflater.inflate(R.layout.fragment_list, container, false)
+
+        if (savedInstanceState!=null) mSubtitleVisible = savedInstanceState.getBoolean(
+            SAVED_SUBTITLE_VISIBLE)
 
         mRecyclerView = view.findViewById(R.id.myRecyclerView)
         mRecyclerView.layoutManager = LinearLayoutManager(context)
@@ -80,6 +115,7 @@ class ListFragment : Fragment() {
             //mUserAdapter?.notifyItemChanged(SingltonUsers.mChange)
             mUserAdapter?.notifyDataSetChanged()
         }
+        updateSubtitle()
 
 
     }
